39d41a5a65e205044b6c8704dfa147616f0aad82
fauzyagilputrandy <fauzyagilputrandy@smkwikrama.sch.id>
2022-05-21 21:19:47 +0700

    imt

--- resources/views/siswa/content/tambah_imt.blade.php
+++ resources/views/siswa/content/tambah_imt.blade.php
@@ -13,7 +13,7 @@
 						<div class="card-deck">
 						<div class="card">
                         <div class="card-body">
-							<form action="" method="post">
+							<form action="" method="get">
                                      <div class="form-group">
                                         <label>Jenis Kelamin</label>
                                         <input type="text" name="jk"  class="form-control text-dark" value="">
@@ -24,11 +24,11 @@
                                     </div>
                                     <div class="form-group">
                                         <label>Tinggi Badang (cm)</label>
-                                        <input type="text" name="tb"   class="form-control text-dark" value="">
+                                        <input type="number" name="tb"   class="form-control text-dark" value="">
                                     </div>
                                     <div class="form-group">
                                         <label>Berat Badan (kg)</label>
-                                        <input type="text" name="rayon"   class="form-control text-dark" value="">
+                                        <input type="number" name="bb"   class="form-control text-dark" value="">
                                     </div>
                                     <div class="form-group">
                                         <label>Semester</label>
@@ -36,9 +36,66 @@
                                     </div>
                                     <div class="row">
                                         <div class="col-6">	
-                                            <button class="btn btn-info form-control">SIMPAN</button>
+                                            <button type="submit" name="proses" class="btn btn-info form-control">SIMPAN</button>
                                         </div>
                                     </div>  
+                                    <?php 
+    if(isset($_GET['proses'])){
+        $tbp = $_GET['tb'];
+        $bb = $_GET['bb'];
+
+        $tb = $tbp/100;
+        $hitung = $bb / ($tb * $tb);
+        if($hitung <= 18.4){
+        echo'
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            Tinggi Badan: '.$tbp.' Cm<br>
+            Berat Badan : '.$bb.' Kg<br>
+            BMI         : '.number_format($hitung,1).'<br>
+            Keterangan : Kurus
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         ';
+        }elseif($hitung <= 25){
+          echo'
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              Tinggi Badan: '.$tbp.' Cm<br>
+              Berat Badan : '.$bb.' Kg<br>
+              BMI         : '.number_format($hitung,1).'<br>
+              Keterangan : Normal
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
+           </div>
+           ';
+        }elseif($hitung <= 27){
+            echo'
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Tinggi Badan: '.$tbp.' Cm<br>
+                Berat Badan : '.$bb.' Kg<br>
+                BMI         : '.number_format($hitung,1).'<br>
+                Keterangan : Gemuk
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            ';
+        }elseif($hitung > 27 ){
+            echo'
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Tinggi Badan: '.$tbp.' Cm<br>
+                Berat Badan : '.$bb.' Kg<br>
+                BMI         : '.number_format($hitung,1).'<br>
+                Keterangan : Obesitas
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            ';
+        }
+    }?>
 							</form>
                           </div>               
 						</div>
